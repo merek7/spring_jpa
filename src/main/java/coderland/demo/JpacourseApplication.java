d16acc01ce04078a7214bce90b68e4e72630d6fa
merek <merek@pop-os.localdomain>
2022-03-17 11:08:11 +0000

    Initialisation du projet et de la basse de donnee

---
+++ src/main/java/coderland/demo/JpacourseApplication.java
@@ -0,0 +1,13 @@
+package coderland.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coderland/demo/JpacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package coderland.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
