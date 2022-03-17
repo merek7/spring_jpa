400acfb2d1380f7884f625cda31de03da07a85ac
merek7 <kmerek7@gmail.com>
2022-03-17 16:44:56 +0000

    creation de l'entite Etudiant.

--- src/main/java/coderland/demo/JpacourseApplication.java
+++ src/main/java/coderland/demo/JpacourseApplication.java
@@ -1,8 +1,9 @@
 package coderland.demo;
 
+import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.hibernate.cfg.Configuration;
 @SpringBootApplication
 public class JpacourseApplication {
 
