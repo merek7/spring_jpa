6ebf1f91332624158a42a8608ea2bd219605091d
merek7 <kmerek7@gmail.com>
2022-03-21 21:02:44 +0000

    nothin

--- src/main/java/coderland/demo/JpacourseApplication.java
+++ src/main/java/coderland/demo/JpacourseApplication.java
@@ -1,14 +1,16 @@
 package coderland.demo;
 
-import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.hibernate.cfg.Configuration;
+
+
+
 @SpringBootApplication
 public class JpacourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpacourseApplication.class, args);
 	}
+
 
 }
